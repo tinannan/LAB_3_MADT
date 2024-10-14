5114593cf1af4060e40545cc37901f842236bdc4
tinannan <yashenkoalevtina@gmail.com>
2024-10-14 10:27:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab3_madt/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lab3_madt;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import org.mariuszgromada.math.mxparser.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Expression e = new Expression("2+3");
+        double v = e.calculate();
+        Toast.makeText(this,String.valueOf(v),Toast.LENGTH_LONG).show();
+    }
+}
